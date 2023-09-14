61e497e46c8db768c60655a9f667837048dfd688
fdg84 <frankwell84@hotmail.com>
2023-09-13 18:27:17 -0700

    Added Colors

--- experiments/drawing-experiments/js/script.js
+++ experiments/drawing-experiments/js/script.js
@@ -22,20 +22,31 @@ function setup() {
 
     createCanvas(1000, 500);
 
-    background(200, 1200, 200);
+    background(222, 222, 222);
 
-    line(0, 0, 685, 775);
-    triangle(40, 95, 78, 40, 96, 93);
-    rectMode(CENTER);
-    rect(250, 250, 100, 100);
-    rect(250, 250, 90, 90);
-    rect(250, 250, 80, 80);
-    rect(250, 250, 70, 70);
-    rect(250, 250, 60, 60);
+    stroke(33, 77,44);
     circle(460, 460, 450);
+
+    rectMode(CENTER);
+    fill(10, 20, 225, 10);
+    rect(220, 220, 100, 100);
+    fill(20, 20, 225, 20);
+    rect(220, 220, 90, 90);
+    fill(30, 20, 225, 30);
+    rect(220, 220, 80, 80);
+    fill(40, 20, 225, 40);
+    rect(220, 220, 70, 70);
+    fill(50, 20, 225, 50);
+    rect(220, 220, 60, 60);
+
+    noStroke();
+    
     ellipseMode(CORNER);
+    fill(107, 20, 225);
     ellipse(350, 350, 100, 100)
+    fill(137, 20, 225);
     ellipse(350, 350, 80, 80)
+    fill(157, 20, 225);
     ellipse(350, 350, 60, 60)
     
 
