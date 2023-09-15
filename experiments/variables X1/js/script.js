1cd90b9c867bca9f51709d9d919feded069c91e5
fdg84 <frankwell84@hotmail.com>
2023-09-15 12:39:34 -0700

    created Variables X1

---
+++ experiments/variables X1/js/script.js
@@ -0,0 +1,88 @@
+/**
+ * Variables
+ * Francis Ouellette
+ * 
+ * Variables class test
+ */
+
+"use strict";
+
+/**
+ * Description of preload
+*/
+function preload() {
+
+}
+
+
+// declare variable
+//let backgroundShade = 0;
+//let circleX = 0;
+//let circleY = 250;
+//let circleSize = 100;
+//let circleSpeed = 2;
+//let circleAcceleration = 0.25;
+
+// variables video 3.4
+//replace variables with property values
+
+let backgroundShade = 0;
+
+let circle = {
+    x: 0,
+    y: 250,
+    size: 100,
+    speed: 2
+}
+
+/**
+ * Description of setup
+*/
+function setup() {
+
+//createCanvas(windowWidth, windowHeight);
+createCanvas(500, 500);
+
+}
+
+
+/**
+ * Description of draw()
+*/
+function draw() {
+
+// variables video 3.1
+    // mute background if you want a trail of squares
+
+ //   background(255, 0, 0);
+    //background(mouseX, mouseY, 0);
+ //   rectMode(CENTER);
+    //rect(250, 250, 100, 100);
+    //rect(mouseX, mouseY, 100, 100);
+    //rect(250, 250, mouseX, mouseY);
+ //   rect(width / 2, height / 2, 100, 100);
+
+
+ // variables video 3.2
+ // variables video 3.3
+ //backgroundShade = backgroundShade + 0.5;
+ //background(backgroundShade);
+ //circleSize = circleSize + 1;
+ //circleY = circleY - 1;
+ //circleX += circleSpeed;
+ //circleSpeed -= circleAcceleration;
+ //ellipse(circleX, circleY, circleSize);
+
+
+// variables video 3.4
+background(backgroundShade);
+circle.x = circle.x + circle.speed;
+ellipse(circle.x, circle.y, circle.size);
+
+// variables video 3.5
+// printing out info if needed to debug project
+//console.log("circleX: " + circleX);
+// same as code below 
+//console.log(`circleX: ${circleX}, circleY: ${circleY}, circleSize: ${circleSize}`);
+// NaN = Not a Number - when undefined value ex: speed
+}
