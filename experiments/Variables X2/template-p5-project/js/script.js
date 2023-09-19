9edf7cbced6f1c180bea61c30ee611b3e625c27a
fdg84 <frankwell84@hotmail.com>
2023-09-19 11:01:25 -0700

    Variables X2 - map and constrain

--- experiments/Variables X2/template-p5-project/js/script.js
+++ experiments/Variables X2/template-p5-project/js/script.js
@@ -1,9 +1,8 @@
 /**
- * Title of Project
- * Author Name
+ * Variables X2 - random numbers
+ * Francis Ouellette
  * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
+ * variables - random numbers!
  */
 
 "use strict";
@@ -15,11 +14,21 @@ function preload() {
 
 }
 
+let backgroundShade = 0;
+let circle = {
+    x: 250,
+    y: 250,
+    size: 100,
+    speed: 1,
+    fill: 0
+}
+
 
 /**
  * Description of setup
 */
 function setup() {
+    createCanvas(500, 500);
 
 }
 
@@ -28,5 +37,18 @@ function setup() {
  * Description of draw()
 */
 function draw() {
+    background(backgroundShade);
+
+    circle.speed = random(-5,5);
+    circle.x = circle.x + circle.speed;
+    circle.y = random(0,height);
+    circle.size = random(10,100);
+
+    circle.fill = random(0,255);
+    fill(circle.fill);
+    ellipse(circle.x, circle.y, circle.size);
+
+    let randomNumber = random();
+    console.log(randomNumber);
 
 }
