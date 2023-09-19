d7be242d217f13fa54ae22dd9a82c7fe4020abcb
fdg84 <frankwell84@hotmail.com>
2023-09-19 11:19:00 -0700

    Variables X2 - moving pictures

--- experiments/Variables X2/template-p5-project/js/script.js
+++ experiments/Variables X2/template-p5-project/js/script.js
@@ -16,11 +16,13 @@ function preload() {
 
 let backgroundShade = 0;
 let circle = {
-    x: 250,
+    //x: 250,
+    x: 0,
     y: 250,
     size: 100,
     speed: 1,
-    fill: 0
+    fill: 255
+    //fill: 0
 }
 
 
@@ -39,16 +41,24 @@ function setup() {
 function draw() {
     background(backgroundShade);
 
-    circle.speed = random(-5,5);
+    //circle.speed = random(-5,5);
     circle.x = circle.x + circle.speed;
-    circle.y = random(0,height);
-    circle.size = random(10,100);
+    circle.x = constrain(circle.x,0,width);
+    //circle.y = random(0,height);
+    //circle.size = random(10,100);
 
-    circle.fill = random(0,255);
+    //circle.fill = random(0,255);
+
+    circle.size = map(mouseY,0,height,10,500);
+    //circle.size = map(mouseY,height,0,10,500);
+
+    //circle.fill = map(circle,0,width,0,255);
+    circle.fill = map(mouseX,0,width,0,255);
+
     fill(circle.fill);
     ellipse(circle.x, circle.y, circle.size);
 
-    let randomNumber = random();
-    console.log(randomNumber);
+    //let randomNumber = random();
+    //console.log(randomNumber);
 
 }
