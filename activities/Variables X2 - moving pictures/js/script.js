8327e815e25f17af63061885a79c42e53587c282
fdg84 <frankwell84@hotmail.com>
2023-09-19 11:22:34 -0700

    Activity 03 - moving pictures

---
+++ activities/Variables X2 - moving pictures/js/script.js
@@ -0,0 +1,31 @@
+/**
+ * Variables X2 - moving pictures
+ * Francis Ouellette
+ * 
+ * activity 03!
+ */
+
+"use strict";
+
+/**
+ * Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+ * Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+ * Description of draw()
+*/
+function draw() {
+
+}
