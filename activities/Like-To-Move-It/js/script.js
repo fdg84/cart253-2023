6b3fa562e7043352b42b2f05b06fd37495fc227b
fdg84 <frankwell84@hotmail.com>
2023-09-21 13:38:49 -0700

    Start - I Like To Move It

---
+++ activities/Like-To-Move-It/js/script.js
@@ -0,0 +1,31 @@
+/**
+ * I Like To Move It
+ * Francis Ouellette
+ * 
+ * New Exercise!
+ */
+
+"use strict";
+
+/**
+ * Description of preload 
+*/
+function preload() {
+
+}
+
+
+/**
+ * Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+ * Description of draw()
+*/
+function draw() {
+
+}
