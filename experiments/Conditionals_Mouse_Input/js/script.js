bf435f43b99ed82ed31a2303247e1d7ef5099af6
fdg84 <frankwell84@hotmail.com>
2023-09-25 20:46:37 -0700

    Mouse Input 4.6 (Conditionals)

---
+++ experiments/Conditionals_Mouse_Input/js/script.js
@@ -0,0 +1,32 @@
+/**
+ * Conditionals - mouse input
+ * Francis Ouellette
+ * 
+ * Test project
+ */
+
+"use strict";
+
+function preload() {
+
+}
+
+let circle = {
+    x: 250,
+    y: 250,
+    size: 100
+}
+
+
+function setup() {
+    createCanvas(500,500);
+
+}
+
+
+function draw() {
+    background(0);
+    ellipse(circle.x,circle.y,circle.size);
+    
+
+}
