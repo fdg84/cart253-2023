91821c9216cbd28fcfb0ad8aaaf6fe53910d1b7c
fdg84 <frankwell84@hotmail.com>
2023-09-25 17:43:05 -0700

    Conditionals - 01

---
+++ experiments/Conditionals/js/script.js
@@ -0,0 +1,31 @@
+/**
+ * Conditionals
+ * Francis Ouellette
+ * 
+ * New test project!
+ */
+
+"use strict";
+
+/**
+ * Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+ * Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+ * Description of draw()
+*/
+function draw() {
+
+}
