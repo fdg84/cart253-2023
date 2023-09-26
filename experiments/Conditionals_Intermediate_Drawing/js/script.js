8401b780fb9fb76bb8be08c66943040276807bf4
fdg84 <frankwell84@hotmail.com>
2023-09-25 21:33:15 -0700

    Intermediate Drawing 4.8 (Conditionals)

---
+++ experiments/Conditionals_Intermediate_Drawing/js/script.js
@@ -0,0 +1,20 @@
+/**
+ * Conditionals - Intermediate Drawing
+ * Francis Ouellette
+ * 
+ * New test!
+ */
+
+"use strict";
+
+function preload() {
+
+}
+
+function setup() {
+
+}
+
+function draw() {
+
+}
