6dbd007714b03c29502e482391092412a4e15844
fdg84 <frankwell84@hotmail.com>
2023-09-25 18:29:20 -0700

    Done 4.1 + 4.2 (Conditionals)

--- experiments/Conditionals/js/script.js
+++ experiments/Conditionals/js/script.js
@@ -16,7 +16,7 @@ let circle = {
     x: 0,
     y: 250,
     size: 100,
-    speed: 5
+    speed: 1
 }
 
 function setup() {
@@ -28,21 +28,40 @@ function draw() {
 
     circle.x = circle.x + circle.speed;
 
+// 4.1 VIDEO
+
     if (circle.x > width) {
         circle.speed = -circle.speed;
     }
-
     if (circle.x < 0) {
         circle.speed = -circle.speed;
     }
-
-    if (mouseY < height/2){
+    if (mouseY < height/2) {
         fill(255,0,0);
     }
-
-    if (mouseY > height/2){
+    if (mouseY > height/2) {
         fill(0,0,255);
     }
+
+// 4.2 VIDEO
+
+    // if (mouseX < width/3) {
+    //     fill(255,0,0);
+    // }
+    // else if (mouseX < 2 * width/3) {
+    //     fill(0,255,0);
+    // }
+    // else {
+    //     fill(0,0,255);
+    // }
+
+            // fill(255,255,255);
+
+            // if (circle.x > width/3 && circle.x < 2 * width/3) {
+            // fill(255,0,0);
+            //     }
+            // }
+
 
     ellipse(circle.x,circle.y,circle.size);
 
@@ -50,4 +69,5 @@ function draw() {
 
 
 // other equations >= <= === !== (relational operators)
+//// && and || or ! not 
 
