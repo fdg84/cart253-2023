3c62a0e83ea60700615f127da9e8cbf3dac1e3d0
fdg84 <frankwell84@hotmail.com>
2023-09-25 21:59:25 -0700

    Done - 4.8 (Drawing)

--- experiments/Conditionals_Intermediate_Drawing/js/script.js
+++ experiments/Conditionals_Intermediate_Drawing/js/script.js
@@ -7,14 +7,56 @@
 
 "use strict";
 
-function preload() {
-
-}
+let angle = 0;
 
 function setup() {
-
+createCanvas(500,500);
 }
 
 function draw() {
+background(155);
 
+// push();
+// fill(255,0,0);
+// stroke(0,255,255);
+// strokeWeight(10);
+// rect(100,100,100,100);
+// pop();
+
+// push();
+// fill(0,0,255);
+// stroke(0);
+// strokeWeight(1);
+// rect(300,100,100,100)
+// pop();
+
+    // push();
+    // fill(255,0,0);
+    // rect(0,0,100,100);
+    // pop();
+
+    // push();
+    // translate(200,100);
+    // fill(0,255,0);
+    // rect(0,0,100,100);
+    // pop();
+
+    // push();
+    // translate(0,200);
+    // fill(0,0,255);
+    // rect(0,0,100,100);
+    // pop();
+
+    push();
+    fill(100,0,0);
+    rectMode(CENTER);
+    translate(width/2,height/2);
+    rotate(angle);
+    // scale(rectScale);
+    rect(0,0,100,100);
+    pop();
+
+    angle = angle + 0.05;
+
+    // rectScale = rectScale + 0.01;
 }
