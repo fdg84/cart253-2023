4c5a278fbdb2f8dc7202642b91988409c14438d3
fdg84 <frankwell84@hotmail.com>
2023-09-25 18:42:41 -0700

    LOOPS video 4.4

--- experiments/Conditionals_Booleans/js/script.js
+++ experiments/Conditionals_Booleans/js/script.js
@@ -10,10 +10,39 @@
 function preload() {
 }
 
+let displayCircle = false;
 
 function setup() {
+    createCanvas(500,500);
 }
 
 
 function draw() {
+
+// 4.3 VIDEO
+
+// keyIsPressed
+    // if (mouseIsPressed) {
+    //     background(200);
+    // }
+    // else {
+    //     background(0);  
+    // }
+
+//     background(0);
+
+// if (mouseIsPressed) {
+//     displayCircle = true;
+// }
+
+// if (displayCircle) {
+//     ellipse(250,250,100,100);
+// }
+
+// 4.4 VIDEO
+
+
+
+    
 }
+
