a1bdfabc6c623b271d541a1e9545ed6ee234a000
fdg84 <frankwell84@hotmail.com>
2023-09-26 13:02:50 -0700

    Time 4.11 (Conditionals)

--- experiments/Conditionals_Images/js/script.js
+++ experiments/Conditionals_Images/js/script.js
@@ -7,14 +7,30 @@
 
 "use strict";
 
+// let clownImage;
+let clown = {
+    x: 250,
+    y: 250,
+    size: 100,
+    image: undefined
+}
+
 function preload() {
+// clownImage = loadImage("C:/Users/T450s/Documents/GitHub/cart253-2023/experiments/Conditionals_Images/assets/images/clown.png")
+clownImage = loadImage("C:/Users/T450s/Documents/GitHub/cart253-2023/experiments/Conditionals_Images/assets/images/clown.png")
 
 }
 
 function setup() {
-
+createCanvas(500,500);
 }
 
 function draw() {
+// background(0);
+
+// imageMode(CENTER);
+// image(clownImage,mouseX,mouseY);
+
+
 
 }
