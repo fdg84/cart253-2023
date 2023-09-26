fa708e7f4bfa1ee99eeff2242f8e43959ea27b08
fdg84 <frankwell84@hotmail.com>
2023-09-25 20:39:10 -0700

    Mouse Input 4.6 (Conditionals)

--- experiments/Conditionals_Booleans/js/script.js
+++ experiments/Conditionals_Booleans/js/script.js
@@ -12,18 +12,52 @@ function preload() {
 
 // let displayCircle = false;
 
-let caterpillar = {
-    x: 100,
+// let caterpillar = {
+//     x: 100,
+//     y: 250,
+//     segmentSize: 50
+// }
+
+let circle = {
+    x: undefined,
+    y: undefined,
+    size: 100
+};
+
+let dangerZone = {
+    x: 250,
     y: 250,
-    segmentSize: 50
-}
+    size: 150
+};
 
 function setup() {
     createCanvas(500,500);
+
+    circle.x = random (0,width);
+    circle.y = random (0,height);
+
+    let d = dist(circle.x,circle.y,dangerZone.x,dangerZone.y);
+    while (d < circle.size/2 + dangerZone.size/2) {
+        circle.x = random (0,width);
+        circle.y = random (0,height);
+        d = dist(circle.x,circle.y,dangerZone.x,dangerZone.y);
+    }
 }
 
 
 function draw() {
+
+    background(0);
+
+    // Danger Zone
+    noFill();
+    stroke(255,0,0);
+    ellipse(dangerZone.x,dangerZone.y,dangerZone.size);
+
+
+    fill(255);
+    noStroke();
+    ellipse(circle.x,circle.y,circle.size);
 
 // 4.3 VIDEO
 
@@ -47,9 +81,9 @@ function draw() {
 
 // 4.4 VIDEO
 
-background(0);
-noStroke();
-fill(100,200,100);
+// background(0);
+// noStroke();
+// fill(100,200,100);
 
 // let x = caterpillar.x;
 // let numSegments = 5;
@@ -62,17 +96,20 @@ fill(100,200,100);
 //  // segmentsDrawn += 1;
 // }
 
-let x = caterpillar.x;
-let numSegments = 10;
+// let x = caterpillar.x;
+// let numSegments = 10;
 
-    // segmentsDrawn = i
+//     // segmentsDrawn = i
 
-for (let i = 0; i < numSegments; i++) {
-    ellipse(x,caterpillar.y,caterpillar.segmentSize);
-    x = x + 40;
-}
+// for (let i = 0; i < numSegments; i++) {
+//     ellipse(x,caterpillar.y,caterpillar.segmentSize);
+//     x = x + 40;
+// }
 
 // 4.5 VIDEO
+
+
+
 
 }
 
