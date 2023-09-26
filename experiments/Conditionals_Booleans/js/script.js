748b5abebb0847e858cf5c080cdf65be263e769d
fdg84 <frankwell84@hotmail.com>
2023-09-25 19:02:41 -0700

    LOOPS - video 4.5

--- experiments/Conditionals_Booleans/js/script.js
+++ experiments/Conditionals_Booleans/js/script.js
@@ -10,7 +10,13 @@
 function preload() {
 }
 
-let displayCircle = false;
+// let displayCircle = false;
+
+let caterpillar = {
+    x: 100,
+    y: 250,
+    segmentSize: 50
+}
 
 function setup() {
     createCanvas(500,500);
@@ -41,8 +47,32 @@ function draw() {
 
 // 4.4 VIDEO
 
+background(0);
+noStroke();
+fill(100,200,100);
 
+// let x = caterpillar.x;
+// let numSegments = 5;
+// let segmentsDrawn = 0;
 
-    
+// while (segmentsDrawn < numSegments) {
+//     ellipse(x,caterpillar.y,caterpillar.segmentSize);
+//     x = x + 40;
+//     segmentsDrawn++;
+//  // segmentsDrawn += 1;
+// }
+
+let x = caterpillar.x;
+let numSegments = 10;
+
+    // segmentsDrawn = i
+
+for (let i = 0; i < numSegments; i++) {
+    ellipse(x,caterpillar.y,caterpillar.segmentSize);
+    x = x + 40;
+}
+
+// 4.5 VIDEO
+
 }
 
