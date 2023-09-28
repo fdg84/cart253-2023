c2978be307c8f0485591a21f08ecab6d33b1e98d
fdg84 <frankwell84@hotmail.com>
2023-09-28 14:18:33 -0700

    Adding Image to Dodge Em

--- experiments/Conditionals_Dodging/js/script.js
+++ experiments/Conditionals_Dodging/js/script.js
@@ -12,75 +12,72 @@ function preload() {
 }
 
 let covid = {
-x: 0,
-y: 250,
-size: 100,
-vx: 0,
-vy: 0,
-speed: 5
-fill: {
-    r: 255,
-    g: 0,
-    b: 0
+    x: 0,
+    y: 250,
+    size: 100,
+    vx: 0,
+    vy: 0,
+    speed: 5,
+    fill: {
+      r: 255,
+      g: 0,
+      b: 0
     }
-
-};
-
-let user = {
+  };
+  
+  let user = {
     x: 250,
     y: 250,
     size: 100,
     fill: 255
-};
-
-let numStatic = 5000; 
-
-function setup() {
-    createCanvas(windowWidth,windowHeight);
-
-    covid.y = random(0,height);
+  };
+  
+//   let numStatic = 5000;
+  
+  function setup() {
+    createCanvas(windowWidth, windowHeight);
+  
+    covid.y = random(0, height);
     covid.vx = covid.speed;
-
+  
     noCursor();
-}
-
-
-function draw() {
+  }
+  
+  function draw() {
     background(0);
-
-    // display static
-    for (let i = 0; i < numStatic; i++) {
-        let x = random(0,width);
-        let y = random(0,height);
-        stroke(255);
-        point(x,y);
-    }
-    
-
-    // covid movement
+  
+    // // Display static
+    // for (let i = 0; i < numStatic; i++) {
+    //   let x = random(0, width);
+    //   let y = random(0, height);
+    //   stroke(255);
+    //   point(x, y);
+    // }
+  
+    // Covid movement
     covid.x = covid.x + covid.vx;
     covid.y = covid.y + covid.vy;
-
+  
     if (covid.x > width) {
-        covid.x = 0;
-        covid.y = random(0,height);
+      covid.x = 0;
+      covid.y = random(0, height);
     }
-
-    // user movement
+  
+    // User movement
     user.x = mouseX;
     user.y = mouseY;
-
-    // check for catching covid
-    let d = dist(user.x,user.y,covid.x,covid.y);
-    if (d < covid.x,covid.y,covid.size);
-        noLoop();
-
-    // display covid
-    fill(circle.fill.r,covid.fill.g,covid.fill.b);
-    ellipse(covid.x,covid.y,covid.size);
-
-    // display user
+  
+    // Check for catching covid
+    let d = dist(user.x, user.y, covid.x, covid.y);
+    if (d < covid.size / 2 + user.size / 2) {
+      noLoop();
+    }
+  
+    // Display covid 
+    fill(covid.fill.r, covid.fill.g, covid.fill.b);
+    ellipse(covid.x, covid.y, covid.size);
+  
+    // Display user
     fill(user.fill);
-    ellipse(user.x,user.y,user.size);
-
-}
+    ellipse(user.x, user.y, user.size);
+  }
