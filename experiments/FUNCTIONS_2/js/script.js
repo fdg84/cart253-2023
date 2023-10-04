7b75c7e838818c953e8c526a00a405c0826251df
fdg84 <frankwell84@hotmail.com>
2023-10-04 10:23:40 -0700

    draw lines

--- experiments/FUNCTIONS_2/js/script.js
+++ experiments/FUNCTIONS_2/js/script.js
@@ -11,9 +11,21 @@ function preload() {
 }
 
 function setup() {
-
+    createCanvas(500,500);
 }
 
 function draw() {
+    background(0);
+
+    let x = 50;
+    let y = 250;
+    for (let i = 0; i < 20; i++) {
+        noStroke();
+        fill(255);
+        rectMode(CENTER);
+        rect(x,y,2,50);
+        x = x + 5;
+    }
+
 
 }
