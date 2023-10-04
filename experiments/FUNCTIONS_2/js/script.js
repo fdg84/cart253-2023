610291f5d990c4c21e719f5ca8898d32a3ef3cca
fdg84 <frankwell84@hotmail.com>
2023-10-04 10:43:43 -0700

    END - added lineSpacing

--- experiments/FUNCTIONS_2/js/script.js
+++ experiments/FUNCTIONS_2/js/script.js
@@ -17,13 +17,13 @@ function setup() {
 function draw() {
     background(0);
 
-    parallels(100,100,5,4,100);
-    parallels(50,50,10,6,33);
-    parallels(200,200,20,5,77);
-    parallels(311,257,7,2,55);
+    parallels(100,100,5,4,100,5);
+    parallels(50,50,10,6,33,7);
+    parallels(200,200,20,5,77,22);
+    parallels(311,257,7,2,55,55);
   }
 
-  function parallels(x,y,numLines,lineWidth,lineHeight) {
+  function parallels(x,y,numLines,lineWidth,lineHeight,lineSpacing) {
     // let x = 50;
     // let y = 250;
     for (let i = 0; i < numLines; i++) {
@@ -31,6 +31,6 @@ function draw() {
         fill(255);
         rectMode(CENTER);
         rect(x,y,lineWidth,lineHeight);
-        x = x + 10;
+        x = x + lineSpacing;
     }
   }
