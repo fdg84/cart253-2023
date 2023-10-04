e57b90f82f72fee8d56931d53c009a9c4b8d50c7
fdg84 <frankwell84@hotmail.com>
2023-10-04 10:15:27 -0700

    ADDED - Move - Wrap - Display

--- experiments/FUNCTIONS_1/js/script.js
+++ experiments/FUNCTIONS_1/js/script.js
@@ -24,13 +24,24 @@ function setup() {
 function draw() {
     background(0);
 
+    move();
+    wrap();
+    display();
+
+}
+
+function move() {
     circle.x = circle.x + circle.vx;
     circle.y = circle.y + circle.vy;
+}
 
+function wrap() {
     if (circle.x > width) {
         reset();
     }
+}
 
+function display() {
     fill(255,0,0);
     ellipse(circle.x,circle.y,circle.size);
 }
@@ -38,7 +49,7 @@ function draw() {
 function reset() {
     circle.x = 0;
     circle.vx = circle.vx + 2;
-    circle.vy = circle.vy - 0.25;
+    //circle.vy = circle.vy - 0.25;
     circle.size = circle.size + 5;
 }
 
