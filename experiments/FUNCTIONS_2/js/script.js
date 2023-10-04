05f90ce7af540a178399ce78dddd914472901b06
fdg84 <frankwell84@hotmail.com>
2023-10-04 10:42:28 -0700

    numLines - lineWidth - lineHeight

--- experiments/FUNCTIONS_2/js/script.js
+++ experiments/FUNCTIONS_2/js/script.js
@@ -17,18 +17,20 @@ function setup() {
 function draw() {
     background(0);
 
-    parallels(100,100);
-    parallels(50,50);
+    parallels(100,100,5,4,100);
+    parallels(50,50,10,6,33);
+    parallels(200,200,20,5,77);
+    parallels(311,257,7,2,55);
   }
 
-  function parallels(x,y) {
-    let x = 50;
-    let y = 250;
-    for (let i = 0; i < 20; i++) {
+  function parallels(x,y,numLines,lineWidth,lineHeight) {
+    // let x = 50;
+    // let y = 250;
+    for (let i = 0; i < numLines; i++) {
         noStroke();
         fill(255);
         rectMode(CENTER);
-        rect(x,y,2,50);
-        x = x + 5;
+        rect(x,y,lineWidth,lineHeight);
+        x = x + 10;
     }
   }
