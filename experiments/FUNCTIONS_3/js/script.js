03d8be66ecb0b7925145597ecfddedee1ac08d79
fdg84 <frankwell84@hotmail.com>
2023-10-04 11:26:37 -0700

    END - return values

--- experiments/FUNCTIONS_3/js/script.js
+++ experiments/FUNCTIONS_3/js/script.js
@@ -6,31 +6,66 @@
 
 "use strict";
 
-function preload() {
+// function preload() {
 
-}
+// }
 
-function setup() {
-    createCanvas(500,500);
+// function setup() {
+//     createCanvas(500,500)
+//     let hotCelsius = toCelsius(100);
+//     console.log(`100 degrees Fahrenheit is ${hotCelsius} degrees Celsius.`);
+  
+//     let coldCelsius = toCelsius(10);
+//     console.log(`10 degrees Fahrenheit is ${coldCelsius} degrees Celsius`);
+//   }
+  
+//   function toCelsius(fahrenheit) {
+//     let result = (fahrenheit - 32) * 5/9;
+//     // Return the value in the celsius variable
+//     return result;
+//   }
 
-    let hotCelsius = toCelsius(100);
-    console.log(`100 degrees Farenheit is ${hotCelsius} degrees Celsius`);
+// CIRCLE TEST
 
-    let coldCelsius = toCelsius(10);
-    console.log(`10 degrees Farenheit is ${coldCelsius} degrees Celsius`);
-}
-
-function draw() {
+let circle = {
+    x: 250,
+    y: 250,
+    size: 100,
+    vx: 0,
+    vy: 0
+  }
+  
+  function setup() {
+    createCanvas(500, 500)
+    reset();
+  }
+  
+  function draw() {
     background(0);
-
-    // let x = random(0,width);
-    // let y = random(0,height);
-    // ellipse(x,y,100,100);
-    // same as below >>
-    // ellipse(random(0,width),random(0,height),100,100);
-
-    function toCelsius(fahrenheit) {
-        let celsius = (fahrenheit - 32) * 5/9;
-        return celsius;
+  
+    move();
+  
+    let offScreen = circleIsOffScreen();
+    if (offScreen) {
+      reset();
     }
-}
+  
+    ellipse(circle.x, circle.y, circle.size);
+  }
+  
+  function circleIsOffScreen() {
+    let result = (circle.x < 0 || circle.x > width || circle.y < 0 || circle.y > height);
+    return result;
+  }
+  
+  function move() {
+    circle.x = circle.x + circle.vx;
+    circle.y = circle.y + circle.vy;
+  }
+  
+  function reset() {
+    circle.x = 250;
+    circle.y = 250;
+    circle.vx = random(-10, 10);
+    circle.vy = random(-10, 10);
+  }
