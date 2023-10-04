0cbad8674c1d0cdb8b562df3139e020df91a999c
fdg84 <frankwell84@hotmail.com>
2023-10-04 10:07:23 -0700

    added mouse pressed

--- experiments/FUNCTIONS_1/js/script.js
+++ experiments/FUNCTIONS_1/js/script.js
@@ -1,20 +1,44 @@
 /**
- * TEMPLATE
+ * Function 1
  * Francis Ouellette
  * 
- * Test!
  */
 
 "use strict";
 
 function preload() {
-
 }
 
-function setup() {
+let circle = {
+    x: 0,
+    y: 250,
+    size: 100,
+    vx: 1,
+    vy: 0
+};
 
+function setup() {
+    createCanvas(500,500);
 }
 
 function draw() {
+    background(0);
 
+    circle.x = circle.x + circle.vx;
+    circle.y = circle.y + circle.vy;
+
+    if (circle.x > width) {
+        circle.x = 0;
+        circle.vx = circle.vx + 2;
+        circle.size = circle.size + 5;
+    }
+
+    fill(255,0,0);
+    ellipse(circle.x,circle.y,circle.size);
+}
+
+function mousePressed() {
+    circle.x = 0;
+    circle.vx = circle.vx + 2;
+    circle.size = circle.size + 5;
 }
