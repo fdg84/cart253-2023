226fc092d49a425118ea9c92b8df9f3855b32ccd
fdg84 <frankwell84@hotmail.com>
2023-10-04 11:15:20 -0700

    Hot & Cold - Celsius / Farenheit

--- experiments/FUNCTIONS_3/js/script.js
+++ experiments/FUNCTIONS_3/js/script.js
@@ -11,9 +11,26 @@ function preload() {
 }
 
 function setup() {
+    createCanvas(500,500);
 
+    let hotCelsius = toCelsius(100);
+    console.log(`100 degrees Farenheit is ${hotCelsius} degrees Celsius`);
+
+    let coldCelsius = toCelsius(10);
+    console.log(`10 degrees Farenheit is ${coldCelsius} degrees Celsius`);
 }
 
 function draw() {
+    background(0);
 
+    // let x = random(0,width);
+    // let y = random(0,height);
+    // ellipse(x,y,100,100);
+    // same as below >>
+    // ellipse(random(0,width),random(0,height),100,100);
+
+    function toCelsius(fahrenheit) {
+        let celsius = (fahrenheit - 32) * 5/9;
+        return celsius;
+    }
 }
