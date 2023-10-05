5b34af9778a339a9448be875dc64e05e4dbd284e
fdg84 <frankwell84@hotmail.com>
2023-10-05 10:39:55 -0700

    Added - Shaking Circle (LOVE)

--- activities/LOVE_ACTUALLY/js/script.js
+++ activities/LOVE_ACTUALLY/js/script.js
@@ -11,9 +11,18 @@ function preload() {
 
 }
 
+let circle1 = {
+    x: 550,
+    y: 150,
+    vx: 0,
+    vy: 0,
+    speed: 5,
+    size: 100
+  }
+
 let circle = {
-    x: 250,
-    y: 250,
+    x: 750,
+    y: 350,
     vx: 0,
     vy: 0,
     speed: 5,
@@ -21,30 +30,30 @@ let circle = {
   }
   
   function setup() {
-    createCanvas(500, 500);
+    createCanvas(1500, 700);
   }
   
   function draw() {
     background(255,0,0);
   
-    // First check for horizontal movement
-    // Is the left arrow pressed?
+    // horizontal movement
+    // left arrow
     if (keyIsDown(LEFT_ARROW)) {
-      // If it is, set the x velocity to be negative
+      // x velocity negative
       circle.vx = -circle.speed;
     }
-    // Otherwise is the right arrow pressed?
+    // right arrow
     else if (keyIsDown(RIGHT_ARROW)) {
-      // If it is, set the x velocity to be positive
+      // x velocity positive
       circle.vx = circle.speed;
     }
-    // If neither of those keys are pressed...
+    // neither pressed
     else {
-      // Then set the x velocity to 0 to stop moving horizontally
+      // x velocity stop moving 
       circle.vx = 0;
     }
   
-    // Do the same thing with vertical movement and the UP and DOWN keys
+    // vertical movement UP DOWN keys
     if (keyIsDown(UP_ARROW)) {
       circle.vy = -circle.speed;
     }
@@ -59,4 +68,13 @@ let circle = {
     circle.y = circle.y + circle.vy;
   
     ellipse(circle.x, circle.y, circle.size);
-  }
+
+      // Choose random velocities within the "speed limit"
+  circle1.vx = random(-circle1.speed, circle1.speed);
+  circle1.vy = random(-circle1.speed, circle1.speed);
+
+  circle1.x = circle1.x + circle1.vx;
+  circle1.y = circle1.y + circle1.vy;
+
+  ellipse(circle1.x, circle1.y, circle1.size);
+}
