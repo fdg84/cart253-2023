c0c4da945b1f7d2d5546ff839e575fa914163bb0
fdg84 <frankwell84@hotmail.com>
2023-10-05 09:44:45 -0700

    Done - Keyboard Input

--- experiments/FUNCTIONS_6/js/script.js
+++ experiments/FUNCTIONS_6/js/script.js
@@ -11,18 +11,54 @@ function preload() {
 
 }
 
-let bg = 0;
+// https://pippinbarr.com/cart253/topics/input/keyboard-input.html
 
-function setup() {
-  createCanvas(500, 500);
-}
-
-function draw() {
-  background(bg);
-}
-
-// keyPressed() is called whenever a key is pressed!
-function keyPressed() {
-  // Set out bg variable to a random number
-  bg = random(0, 255);
-}
+let circle = {
+    x: 250,
+    y: 250,
+    vx: 0,
+    vy: 0,
+    speed: 5,
+    size: 100
+  }
+  
+  function setup() {
+    createCanvas(500, 500);
+  }
+  
+  function draw() {
+    background(0);
+  
+    // First check for horizontal movement
+    // Is the left arrow pressed?
+    if (keyIsDown(LEFT_ARROW)) {
+      // If it is, set the x velocity to be negative
+      circle.vx = -circle.speed;
+    }
+    // Otherwise is the right arrow pressed?
+    else if (keyIsDown(RIGHT_ARROW)) {
+      // If it is, set the x velocity to be positive
+      circle.vx = circle.speed;
+    }
+    // If neither of those keys are pressed...
+    else {
+      // Then set the x velocity to 0 to stop moving horizontally
+      circle.vx = 0;
+    }
+  
+    // Do the same thing with vertical movement and the UP and DOWN keys
+    if (keyIsDown(UP_ARROW)) {
+      circle.vy = -circle.speed;
+    }
+    else if (keyIsDown(DOWN_ARROW)) {
+      circle.vy = circle.speed;
+    }
+    else {
+      circle.vy = 0;
+    }
+  
+    circle.x = circle.x + circle.vx;
+    circle.y = circle.y + circle.vy;
+  
+    ellipse(circle.x, circle.y, circle.size);
+  }
