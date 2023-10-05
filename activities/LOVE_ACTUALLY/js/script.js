919ecd474579348cfc9de683cf7e3388280f91e3
fdg84 <frankwell84@hotmail.com>
2023-10-05 11:17:57 -0700

    LOVE - break before class edits

--- activities/LOVE_ACTUALLY/js/script.js
+++ activities/LOVE_ACTUALLY/js/script.js
@@ -1,22 +1,16 @@
 /**
- * New Activity
+ * Class Activity
  * Francis Ouellette
  * 
  * Love Actually!
  */
-
-"use strict";
-
-function preload() {
-
-}
 
 let circle1 = {
     x: 550,
     y: 150,
     vx: 0,
     vy: 0,
-    speed: 5,
+    speed: 10,
     size: 100
   }
 
@@ -35,41 +29,31 @@ let circle = {
   
   function draw() {
     background(255,0,0);
-  
-    // horizontal movement
-    // left arrow
-    if (keyIsDown(LEFT_ARROW)) {
-      // x velocity negative
-      circle.vx = -circle.speed;
-    }
-    // right arrow
-    else if (keyIsDown(RIGHT_ARROW)) {
-      // x velocity positive
-      circle.vx = circle.speed;
-    }
-    // neither pressed
-    else {
-      // x velocity stop moving 
-      circle.vx = 0;
-    }
-  
-    // vertical movement UP DOWN keys
-    if (keyIsDown(UP_ARROW)) {
-      circle.vy = -circle.speed;
-    }
-    else if (keyIsDown(DOWN_ARROW)) {
-      circle.vy = circle.speed;
-    }
-    else {
-      circle.vy = 0;
-    }
-  
-    circle.x = circle.x + circle.vx;
-    circle.y = circle.y + circle.vy;
-  
-    ellipse(circle.x, circle.y, circle.size);
 
-      // Choose random velocities within the "speed limit"
+        if (keyIsDown(LEFT_ARROW)) {
+        circle.vx = -circle.speed;
+        }
+        else if (keyIsDown(RIGHT_ARROW)) {
+        circle.vx = circle.speed;
+        }
+        else {
+        circle.vx = 0;
+        }
+        if (keyIsDown(UP_ARROW)) {
+        circle.vy = -circle.speed;
+        }
+        else if (keyIsDown(DOWN_ARROW)) {
+        circle.vy = circle.speed;
+        }
+        else {
+        circle.vy = 0;
+        }
+    
+        circle.x = circle.x + circle.vx;
+        circle.y = circle.y + circle.vy;
+    
+        ellipse(circle.x, circle.y, circle.size);
+
   circle1.vx = random(-circle1.speed, circle1.speed);
   circle1.vy = random(-circle1.speed, circle1.speed);
 
@@ -77,4 +61,34 @@ let circle = {
   circle1.y = circle1.y + circle1.vy;
 
   ellipse(circle1.x, circle1.y, circle1.size);
+
+  circle1.x = constrain(circle1.x, 0, width);
+  circle1.y = constrain(circle1.y, 0, width);
 }
+
+        if (state === `lovebug`) {
+        lovebug();
+        }
+        else if (state === `captured`) {
+        captured();
+        }
+  
+  function lovebug() {
+    checkOverlap();
+    }
+  
+  function captured() {
+    push();
+    textSize(64);
+    fill(255);
+    textAlign(CENTER,CENTER);
+    text(`CAPTURED THE LOVE BUG!`,width/2,height/2);
+    pop();
+    }
+
+  function checkOverlap() {
+    let d = dist(circle1.x,circle1.y,circle.x,circle.y);
+    if (d < circle1.size/2 + circle.size/2) {
+      state = `captured`;
+    }
+  }
