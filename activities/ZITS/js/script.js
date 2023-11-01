49ae2bf00fb9f2c41a82868f3347bee34a403a52
fdg84 <frankwell84@hotmail.com>
2023-10-31 19:07:48 -0700

    ZITS - tests

--- activities/ZITS/js/script.js
+++ activities/ZITS/js/script.js
@@ -18,26 +18,31 @@ let garden = {
   // How many bees in the garden
   numBees: 5,
   // The color of the grass (background)
-
+  grassColor: {
+    r: 120,
+    g: 180,
+    b: 120
+  }
 };
 
 // setup() creates the canvas and the flowers in the garden
 function setup() {
-  createCanvas(windowWidth, WindowHeight);
+  createCanvas(600, 600);
 
   // Create our flowers by counting up to the number of the flowers
   for (let i = 0; i < garden.numFlowers; i++) {
     // Create variables for our arguments for clarity
     let x = random(0, width);
     let y = random(0, height);
-    let size = random(10, 50);
+    let size = random(50, 80);
+    let stemLength = random(50, 100);
     let petalColor = {
-      r: (255),
-      g: (0),
-      b: (0)
+      r: random(100, 255),
+      g: random(100, 255),
+      b: random(100, 255)
     }
     // Create a new flower using the arguments
-    let flower = new Acne(x, y, size, petalColor);
+    let flower = new Flower(x, y, size, stemLength, petalColor);
     // Add the flower to the array of flowers
     garden.flowers.push(flower);
   }
@@ -48,7 +53,7 @@ function setup() {
     let x = random(0, width);
     let y = random(0, height);
     // Create a new bee using the arguments
-    let bee = new Cream(x, y);
+    let bee = new Bee(x, y);
     // Add the bee to the array of bees
     garden.bees.push(bee);
   }
@@ -58,6 +63,8 @@ function setup() {
 // draw()
 // Displays our flowers
 function draw() {
+  // Display the grass
+  background(garden.grassColor.r, garden.grassColor.g, garden.grassColor.b);
 
   // Loop through all the flowers in the array and display them
   for (let i = 0; i < garden.flowers.length; i++) {
@@ -79,12 +86,12 @@ function draw() {
       bee.shrink();
       bee.move();
 
-      // NEW! Go through the entire flower array and try to Soothe the flowers!
+      // NEW! Go through the entire flower array and try to pollinate the flowers!
       // Note that we use j in our for-loop here because we're already inside
       // a for-loop using i!
       for (let j = 0; j < garden.flowers.length; j++) {
         let flower = garden.flowers[j];
-        bee.tryToSoothe(flower);
+        bee.tryToPollinate(flower);
       }
 
       // Display the bee
