c76f7d1a29b6122a83c3445f2537c146e79286d5
fdg84 <frankwell84@hotmail.com>
2023-11-09 10:28:40 -0800

    MOVEMENT - works (test)

--- prototype/beep-boop/js/script.js
+++ prototype/beep-boop/js/script.js
@@ -20,9 +20,17 @@ function setup() {
 
 function draw() {
   background(0);
+
+  // We can calculate the rate we should set the sound to by mapping
+  // the mouse's x position to our desired range..
+  let newRate = map(mouseX, 0, width, -3, 3);
+  // And then set the rate of our sound file to the new rate
+  // Note how we can do this while the sound is still playing!
+  bellsSFX.rate(newRate);
 }
 
 function mousePressed() {
-  bellsSFX.rate(-1);
-  bellsSFX.play();
+  // Since we'll be playing around with the sound, it makes sense to loop it
+  // so it doesn't stop.
+  bellsSFX.loop();
 }
---
+++ prototype/movement/js/script.js
@@ -0,0 +1,72 @@
+/**
+ * I LIKE TO MOVE IT!
+ * FRANCIS OUELLETTE    
+ * 
+ * NEW EXERCISE
+ */
+
+"use strict";
+
+/**
+ * Description of preload
+*/
+let bellsSFX;
+
+function preload() {
+  bellsSFX = loadSound(`assets/sounds/bellsfx.wav`);
+}
+
+let bg = 0;
+let bgCanvas = 700;
+let size;
+let circle = 0;
+
+
+
+/**
+ * Description of setup
+*/
+function setup() {
+    createCanvas(bgCanvas, bgCanvas);
+    userStartAudio();
+
+}
+
+
+/**
+ * Description of draw()
+*/
+function draw() {
+
+background(140, 100, mouseY);
+circle = map(mouseX, 0, width, 255, 150);
+size = constrain(mouseX, 0, width);
+stroke(200, 50, 100, 100);
+fill(circle, 150, 0);
+ellipse(width/2, height/2, size);
+
+let newRate = map(mouseX, 0, width, -3, 3);
+bellsSFX.rate(newRate);
+
+// fill(25);
+// triangle(250, mouseX, 400, mouseY, 150, 275);
+
+// fill(222, 222, 0)
+// quad(150, mouseX, 555, 250, 350, 138, mouseY, 150);
+
+rectMode(CENTER);
+fill(255);
+rect(350, 350, 10, mouseX);
+
+rectMode(CENTER);
+fill(255);
+rect(350, 350, mouseY, 10);
+
+// let x1 = map(mouseX, 0, width, 0, 700);
+//   ellipse(x1, 55, 55, 55);
+
+}
+
+function mousePressed() {
+  bellsSFX.loop();
+}
